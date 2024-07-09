3ef2a3364d13ed307d9369d9e3c63cc536d296a4
eduardoKrakhecke <eduardokg.mi6@gmail.com>
2024-07-09 15:33:42 -0300

    ajustes configs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+const DEFAULT_ROUTE = '/home';
+
+export const routes: Routes = [
+  { path: '', redirectTo: DEFAULT_ROUTE, pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: '**', redirectTo: DEFAULT_ROUTE, pathMatch:'full' },
+
+];
