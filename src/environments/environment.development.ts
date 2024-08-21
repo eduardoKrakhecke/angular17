cd4352727e7089980e12c66feeb6ecce24d1da35
eduardoKrakhecke <eduardokg.mi6@gmail.com>
2024-08-21 15:23:55 -0300

    ajustes

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  BASE_URL: 'https://tools.texoit.com/backend-java/api/movies',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  BASE_URL: 'https://tools.texoit.com/backend-java/api/movies',
+};
